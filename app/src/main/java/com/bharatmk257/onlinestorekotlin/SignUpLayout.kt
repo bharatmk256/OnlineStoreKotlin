4b9da10de5103a578902ba58cfbdce35b1c48ef9
bharatmk257 <bharatmk257@gmail.com>
2019-03-15 21:06:01 +0530

    project created also UI for Signup and Signin added

---
+++ app/src/main/java/com/bharatmk257/onlinestorekotlin/SignUpLayout.kt
@@ -0,0 +1,12 @@
+package com.bharatmk257.onlinestorekotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_up_layout)
+    }
+}
