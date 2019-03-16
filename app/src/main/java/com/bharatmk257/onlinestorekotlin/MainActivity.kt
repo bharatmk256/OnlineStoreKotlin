26a5e6aa6ec2855b3c4258a724c908fe0a6eeac1
bharatmk257 <bharatmk257@gmail.com>
2019-03-16 12:18:51 +0530

    login process completed

--- app/src/main/java/com/bharatmk257/onlinestorekotlin/MainActivity.kt
+++ app/src/main/java/com/bharatmk257/onlinestorekotlin/MainActivity.kt
@@ -1,7 +1,15 @@
 package com.bharatmk257.onlinestorekotlin
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,7 +17,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        activity_main_btnLogin.setOnClickListener {
 
+            val logitnURL = "http://192.168.0.104/OnlineStoreApp/login_app_user.php?email=" +
+                    activity_main_edtEmail.text.toString() + "&pass=" +
+                    activity_main_edtPassword.text.toString()
+
+            val requestQ = Volley.newRequestQueue(this@MainActivity)
+            val stringRequest = StringRequest(Request.Method.GET, loginURL, Response.Listener { response ->
+
+                if (response.equals("The user does exist")) {
+                    Toast.makeText(this@MainActivity, "You are logged in", Toast.LENGTH_SHORT).show()
+                } else {
+                    val dialogBuilder = AlertDialog.Builder(this)
+                    dialogBuilder.setTitle("Alert")
+                    dialogBuilder.setMessage(response)
+                    dialogBuilder.show()
+                }
+
+            }, Response.ErrorListener { error ->
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setTitle("Alert")
+                dialogBuilder.setMessage(error.message)
+                dialogBuilder.show()
+            })
+
+            requestQ.add(stringRequest)
+
+        }
+
+        activity_main_btnSignUp.setOnClickListener {
+
+            var signUpIntent = Intent(this@MainActivity, SignUpLayout::class.java)
+            startActivity(signUpIntent)
+
+        }
 
     }
 }
--- app/src/main/java/com/bharatmk257/onlinestorekotlin/SignUpLayout.kt
+++ app/src/main/java/com/bharatmk257/onlinestorekotlin/SignUpLayout.kt
@@ -1,12 +1,67 @@
 package com.bharatmk257.onlinestorekotlin
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.sign_up_layout.*
 
 class SignUpLayout : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up_layout)
+
+        sign_up_layout_btnSignIn.setOnClickListener {
+            var signInIntent = Intent(this@SignUpLayout, MainActivity::class.java)
+            startActivity(signInIntent)
+        }
+
+        sign_up_layout_btnSignUp.setOnClickListener {
+
+            if (sign_up_layout_edtPassword.text.toString().equals(sign_up_layout_edtConfirmPassword.text.toString())) {
+
+                // registration process
+
+                val signUpURL =
+                    "http://192.168.0.104/OnlineStoreApp/join_new_user.php?email=" +
+                            sign_up_layout_edtEmail.text.toString() + "&username=" +
+                            sign_up_layout_edtUsername.text.toString() + "&pass=" +
+                            sign_up_layout_edtPassword.text.toString()
+                val requestQ = Volley.newRequestQueue(this@SignUpLayout)
+                val stringRequest = StringRequest(Request.Method.GET, signUpURL, Response.Listener { response ->
+                    if (response.equals("A user with this Email Address already exist")){
+                        val dialogBuilder = AlertDialog.Builder(this)
+                        dialogBuilder.setTitle("Alert")
+                        dialogBuilder.setMessage(response)
+                        dialogBuilder.show()
+                    }else{
+                        val dialogBuilder = AlertDialog.Builder(this)
+                        dialogBuilder.setTitle("Alert")
+                        dialogBuilder.setMessage(response)
+                        dialogBuilder.show()
+                    }
+
+                }, Response.ErrorListener { error ->
+                    val dialogBuilder = AlertDialog.Builder(this)
+                    dialogBuilder.setTitle("Alert")
+                    dialogBuilder.setMessage(error.message)
+                    dialogBuilder.show()
+
+                })
+
+                requestQ.add(stringRequest)
+
+            } else {
+                val dialogBuilder = AlertDialog.Builder(this)
+                dialogBuilder.setTitle("Alert")
+                dialogBuilder.setMessage("Password Mismatch")
+                dialogBuilder.show()
+            }
+        }
     }
 }
